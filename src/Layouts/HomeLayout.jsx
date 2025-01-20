411e79763a8ca2a45947bd13a7fd1eee424669cf
MirazZim <miraz.zim.38@gmail.com>
2025-01-20 18:13:22 +0600

    Router setup and folder structure analysis

---
+++ src/Layouts/HomeLayout.jsx
@@ -0,0 +1,15 @@
+import Header from "../Components/Header";
+
+
+const HomeLayout = () => {
+    return (
+        <div>
+            <header>
+                <Header></Header>
+            </header>
+            
+        </div>
+    );
+};
+
+export default HomeLayout;
--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+
+import { RouterProvider } from "react-router-dom";
+import router from './routes/router.jsx';
 
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <div>Hello world!</div>,
-  },
-]);
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    
+
     <RouterProvider router={router} />
   </StrictMode>,
 )
